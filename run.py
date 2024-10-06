767a27690d0ec3887085af747873ef3d1025f5e2
jcarvajal14 <99988995+jcarvajal14@users.noreply.github.com>
2024-10-05 23:52:35 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -3,11 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+
 def home():
     return render_template('index.html')
 
+@app.route('/mundo')
+
 def mundo():
-    return render_template('index.html')
+    return render_template('mundo.html')
+
+@app.route('/manual')
+
+def manual():
+    return render_template('manual.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
